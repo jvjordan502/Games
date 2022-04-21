b95ff4035557f1fdfcd772ac6decf4dac5bb7e25
jvjordan502 <104048339+jvjordan502@users.noreply.github.com>
2022-04-21 16:57:03 -0400

    Add files via upload

--- SpellingBee.py
+++ SpellingBee.py
@@ -1,7 +1,8 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 20 17:27:12 2022
+Created on Wed Apr 20 15:27:12 2022
 
 @author: james
 """
@@ -22,8 +23,9 @@ print(len(refined_list))
 
 new_list = [x for x in refined_list if (letters[1].lower() in x or letters[2].lower() in x or letters[3].lower() in x or letters[4].lower() in x or letters[5].lower() in x or letters[6].lower() in x )and len(x) > 3]
 print(len(new_list))
-bad_list = [x for x in new_list if remainders[0].lower() in x or remainders[1].lower() in x or remainders[2].lower() in x or remainders[3].lower() in x or remainders[4].lower() in x or remainders[5].lower() in x or remainders[6].lower() in x or remainders[7].lower() in x or remainders[8].lower() in x or remainders[9].lower() in x or remainders[10].lower() in x or remainders[11].lower() in x or remainders[12].lower() in x or remainders[13].lower() in x or remainders[14].lower() in x or remainders[15].lower() in x or remainders[16].lower() in x or remainders[17].lower() in x or remainders[18].lower() in x]
+
+for i in range(len(remainders)):
+    new_list = [x for x in new_list if remainders[i].lower() not in x ]
     
-final_list = list(set(new_list)-set(bad_list))
-print(len(final_list))
-print(final_list)
+print(sorted(new_list))
+print(len(new_list))
